c89256a5e7fd1f4dc5740036280e2b8659dc8a66
Balerina <katarzynab9236@gmail.com>
2018-10-18 23:44:00 -0500

    js push

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Train-HW-Firebase
+
+# Purpose
+
+The purpose of this app is to create a train scheduler utilizing firebase to store data. This way people in the real world can search train arrival and departure information based on a specific destination. This website will provide up-to-date information about various trains, namely their arrival times and how many minutes remain until they arrive at their station.
+
+# Who will use this app?
+
+Anyone who is travelling by train or who needs to pick up someone travelling by train.
+
+# What is the goal?
+
+The goal of this project is create a useful app where users can search train information for their travels. The app achieves this by utilizing firebase to store train data and Moment.js to parse, validate, manipulate, and display dates and times in JavaScript.
+
+# Instructions
+  
+In order to use the app, users need to fill out the following information:  
+    * Train Name
+    * Destination 
+    * First Train Time -- in military time
+    * Frequency -- in minutes
+Then the next train arrival will be displayed relative to the current time.
+Enjoy!
+  
+ 
+  
---
+++ schedule.js
@@ -0,0 +1,127 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCRxHDfTf86UohhuwJZLbHRhwol6bPua2E",
+    authDomain: "trainsceduler-c23fe.firebaseapp.com",
+    databaseURL: "https://trainsceduler-c23fe.firebaseio.com",
+    projectId: "trainsceduler-c23fe",
+    storageBucket: "",
+    messagingSenderId: "5597386602"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+// 2. Button for adding Employees
+$("#add-employee-btn").on("click", function (event) {
+    event.preventDefault();
+
+    // Grabs user input
+    var empName = $("#employee-name-input").val().trim();
+    var empRole = $("#role-input").val().trim();
+    var empStart = moment($("#start-input").val().trim(), "MM/DD/YYYY").format("X");
+    var empRate = $("#rate-input").val().trim();
+
+    // Creates local "temporary" object for holding employee data
+    var newEmp = {
+        name: empName,
+        role: empRole,
+        start: empStart,
+        rate: empRate
+    };
+
+    // Uploads employee data to the database
+    database.ref().push(newEmp);
+
+    // Logs everything to console
+    console.log(newEmp.name);
+    console.log(newEmp.role);
+    console.log(newEmp.start);
+    console.log(newEmp.rate);
+
+    alert("Employee successfully added");
+
+    // Clears all of the text-boxes
+    $("#employee-name-input").val("");
+    $("#role-input").val("");
+    $("#start-input").val("");
+    $("#rate-input").val("");
+});
+
+// 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+database.ref().on("child_added", function (childSnapshot) {
+    console.log(childSnapshot.val());
+
+    // Store everything into a variable.
+    var empName = childSnapshot.val().name;
+    var empRole = childSnapshot.val().role;
+    var empStart = childSnapshot.val().start;
+    var empRate = childSnapshot.val().rate;
+
+    // Employee Info
+    console.log(empName);
+    console.log(empRole);
+    console.log(empStart);
+    console.log(empRate);
+
+    // Assume the following situations.
+
+    // (TEST 1)
+    // First Train of the Day is 3:00 AM
+    // Assume Train comes every 3 minutes.
+    // Assume the current time is 3:16 AM....
+    // What time would the next train be...? (Use your brain first)
+    // It would be 3:18 -- 2 minutes away
+
+    // (TEST 2)
+    // First Train of the Day is 3:00 AM
+    // Assume Train comes every 7 minutes.
+    // Assume the current time is 3:16 AM....
+    // What time would the next train be...? (Use your brain first)
+    // It would be 3:21 -- 5 minutes away
+
+
+    // ==========================================================
+
+    // Solved Mathematically
+    // Test case 1:
+    // 16 - 00 = 16
+    // 16 % 3 = 1 (Modulus is the remainder)
+    // 3 - 1 = 2 minutes away
+    // 2 + 3:16 = 3:18
+
+    // Solved Mathematically
+    // Test case 2:
+    // 16 - 00 = 16
+    // 16 % 7 = 2 (Modulus is the remainder)
+    // 7 - 2 = 5 minutes away
+    // 5 + 3:16 = 3:21
+
+    // // Assumptions
+    // var tFrequency = 3;
+
+    // // Time is 3:30 AM
+    // var firstTime = "03:30";
+
+    // // First Time (pushed back 1 year to make sure it comes before current time)
+    // var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    // console.log(firstTimeConverted);
+
+    // // Current Time
+    // var currentTime = moment();
+    // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    // // Difference between the times
+    // var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    // console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // // Time apart (remainder)
+    // var tRemainder = diffTime % tFrequency;
+    // console.log(tRemainder);
+
+    // // Minute Until Train
+    // var tMinutesTillTrain = tFrequency - tRemainder;
+    // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // // Next Train
+    // var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    // console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
